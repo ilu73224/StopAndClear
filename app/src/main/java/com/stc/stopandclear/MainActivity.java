f6ae3cc10a1027d409ccc87808886f4d4864a404
ilu73224 <ilu73224@gmail.com>
2017-03-25 21:35:57 +0800

    1.use finishAndRemoveTask() instead of finish() to exit activity. 2.add try to remove application from recent app list, but not work correctly.

--- app/src/main/java/com/stc/stopandclear/MainActivity.java
+++ app/src/main/java/com/stc/stopandclear/MainActivity.java
@@ -1,19 +1,20 @@
 package com.stc.stopandclear;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     private static final String POKEMON_PKG_NAME = "com.nianticlabs.pokemongo";
     private static final int MS_TIME_TO_CLOSE = 1500;
+    private static final String TAG = "StopAndClear";
+    private static final int INVALID_STACKID = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,11 +44,47 @@ public class MainActivity extends AppCompatActivity {
             Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
                 public void run() {
-                    finish();
+                    finishAndRemoveTask();
                 }
             }, MS_TIME_TO_CLOSE);
         }
+        void tryClearInRecentApp(){
+            try {
+                // Run the command
+                Process pGetStackList = Runtime.getRuntime().exec("su shell am stack list");
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(pGetStackList.getInputStream()));
 
+                // Grab the results
+                StringBuilder log = new StringBuilder();
+                String line;
+                int pokemonStackId = INVALID_STACKID;
+                int tmpStackId=INVALID_STACKID;
+                String sPrefix = "Stack id=";
+                String sPostfix = " bounds=[";
+                while ((line = bufferedReader.readLine()) != null) {
+                    if(line.contains(sPrefix)){
+                        String sTmpStackId = line.substring(line.indexOf(sPrefix)+sPrefix.length(),
+                                line.indexOf(sPostfix));
+                        tmpStackId = Integer.parseInt(sTmpStackId);
+                        Log.d(TAG, String.format("tmpStackId = %d\n", tmpStackId));
+                    } else if (line.contains(POKEMON_PKG_NAME)){
+                        pokemonStackId = tmpStackId;
+                        Log.d(TAG, String.format("get %s, tmpStackId = %d, pokemonStackId = %d\n",
+                                POKEMON_PKG_NAME, tmpStackId, pokemonStackId));
+                    }
+                    log.append(String.format("%s\n", line));
+                }
+                Log.d(TAG, "am stack list report :\n " + log.toString());
+                Log.d(TAG, "pokemonStackId = " + pokemonStackId);
+                if(INVALID_STACKID != pokemonStackId){
+                    Process pClearCache = Runtime.getRuntime().exec("su shell am stack remove " + pokemonStackId);
+                    pClearCache.waitFor();
+                }
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
